799d813390facd4478908eded0fdcb9ddb4e2597
Семен Журавлев <zsocom@mail.ru>
2025-04-10 20:44:19 +0300

    Добавление реализации калькулятора
    Реализована задача calculator для ветки feature/calc

--- src/classwork/main.py
+++ src/classwork/main.py
@@ -2,3 +2,35 @@ print("Hello World")
 print("Hello World 2")
 print("Hello World 3")
 print("Hello World 4")
+from function import getMathFunc as getMath
+
+def SplitExample(example : str) -> list:
+    temp = ""
+    first = 0
+    sign = ""
+    second = 0
+    for char in example:
+        if char in "+-*/^":
+            sign = char
+            first = int(temp)
+            temp = ""
+        else:
+            temp += char
+    second = int(temp)
+    return [first, sign, second]
+
+def Equales(example : str) -> int:
+    exList = SplitExample(example)
+    func = getMath(exList[1])
+    return func(exList[0],exList[2])
+def InterFib(number : int) -> int:
+    first = 0
+    second = 1
+    for i in range(2, number + 1):
+        second = first + second
+        first = second - first
+    return second
+
+
+ex = input("Введите пример: ")
+print(f"Ответ: {ex}={Equales(ex)}")

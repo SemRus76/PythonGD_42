e6c6a29941cb6975e4c694629f40920d1a44bfe2
Семен Журавлев <zsocom@mail.ru>
2025-04-15 19:56:22 +0300

    Добавление сабмодуля function

--- src/classwork/main.py
+++ src/classwork/main.py
@@ -2,7 +2,7 @@ print("Hello World")
 print("Hello World 2")
 print("Hello World 3")
 print("Hello World 4")
-from function import getMathFunc as getMath
+from function.function import getMathFunc as getMath
 
 def SplitExample(example : str) -> list:
     temp = ""

afa225b164c5fe8bacbc4a1f9828670db70d574e
Семен Журавлев <zsocom@mail.ru>
2025-04-29 21:55:12 +0300

    Работа с файлами

--- src/classwork/main.py
+++ src/classwork/main.py
@@ -1,109 +1,42 @@
-# Задача 1 - Вводится строка, состоящая из скобочек.
-# Необходимо написать функцию которая волидирует данную строку на правильность
-# Правила:
-# 1. Могут использоватся три вида скобочек - () [] {}
-# 2. У каждой открывающейся ОБЯЗАНА быть закрывающаяся
-# 3. Каждая скобочка ОБЯЗАНА закрываться скобкой своего вида
-# 4. Области действия скобок НЕ МОГУТ пересекаться - ({)} -> False
-# Примеры:
-# ()[]{} -> True
-# ({[]}) -> True
-# ([) -> False
-# ({)} -> False
-# ([]{()}) -> True
+from function.function import getMathFunc as mFunc
+import os
 
-def isBracketValidator(example : str) -> bool:
-    stack = list()
-    for elem in example:
-        if elem in "({[":
-            stack.append(elem)
-        elif elem == ")":
-            if stack[len(stack) - 1] == "(":
-                stack.pop()
-                continue
-            else:
-                return False
-        elif elem == "}":
-            if stack[len(stack) - 1] == "{":
-                stack.pop()
-                continue
-            else:
-                return False
-        elif elem == "]":
-            if stack[len(stack) - 1] == "[":
-                stack.pop()
-                continue
-            else:
-                return False
-    return False if len(stack) else True
+def getResult(example : str) -> int:
+    First = ""
+    Second = ""
+    Sign = ""
+    temp = ""
+    for char in example:
+        if char in "+-*/^":
+            First = int(temp)
+            Sign = char
+            temp = ""
+        elif char in "\n":
+            continue
+        else:
+            temp += char
+    Second = int(temp)
+    func = mFunc(Sign)
+    return func(First, Second)
 
+def getFile():
+    resultLst = list()
+    # file = open("resources/name.txt", "r+")
+    with open("name.txt", "r+") as file:
+        for line in file:
+            line = line.removesuffix("\n")
+            result = getResult(line)
+            resultLst.append(f"{line} = {result}\n")
 
-print(isBracketValidator("{(})"))
+    return resultLst
 
-# Задача 2 - RomanToInt
-# Дана строка, в которой содержится 1 число, записанное римскими цифрами
-# Необходимо вывести строку вида: {римская запись} = {арабская запись}
-# Римские цифры имеют следующие правила
-# I - 1   D = 500
-# V - 5   M = 1000
-# X - 10
-# L - 50
-# C - 100
-# НО, для описания некоторых чисел вводится операция вычитания - IV = 4
-# У римских чисел имеются следующие правила:
-# При записи числа ВСЕГДА впереди идет самый большой порядок, за исключением
-# следующих ситуаций
-# 1. Перед C и L может быть записано несколько X для обозначения вычитания
-# 2. Перед X может быть записано V или I для обозначения вычитания
-# 3. Перед V может быть записано I для обозначения вычитания
-# Примеры для расшифроки:
-# CLXVIII = 168
-# CXLIV = 144
-# LXXXVIII = 88
+os.chdir("resources")
+results = getFile()
+if not os.path.isdir("result"):
+    os.mkdir("result")
 
-def romanToInt(roman : str) -> int:
-    arabian = list()
-    for elem in roman:
-        match elem:
-            case "I":
-                arabian.append(1)
-            case "V":
-                arabian.append(5)
-            case "X":
-                arabian.append(10)
-            case "L":
-                arabian.append(50)
-            case "C":
-                arabian.append(100)
-    result = 0
-    lvl = 1
-    temp = 0
-    for arab in arabian[::-1]:
-        if arab == lvl:
-            temp += lvl
-        elif arab < lvl:
-            temp -= arab
-        elif arab > lvl:
-            result += temp
-            temp = lvl = arab
-    result += temp
-    return result
-    # romanToDigit = {
-    #     "I" : 1,
-    #     "V" : 5,
-    #     "X" : 10,
-    #     "L" : 50,
-    #     "C" : 100
-    # }
-    # levels = "IVXLC"
-    # lvl = 0
-    # temp = 0
-    # result = 0
-    # for elem in roman[::-1]:
-    #     pass
-    # result += temp
-    # return result
-
-print(romanToInt("IVXCVXXL"))
-
-
+os.chdir("result")
+# os.kill(9288,9)
+with open("results.txt", "w") as file:
+    for line in results:
+        file.write(line)

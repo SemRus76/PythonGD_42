4217acce9a75c86c27023bffd91eeaf1674bcb2e
Семен Журавлев <zsocom@mail.ru>
2025-05-27 21:47:00 +0300

    Тема Наследования

--- src/classwork/person.py
+++ src/classwork/person.py
@@ -1,20 +1,9 @@
 # class <Имя класса>:
-from overloading import overload
 
 class Person:
-    @overload
-    def __init__(self):
-        self.__name = None
-        self.__age = None
-
-    @overload
     def __init__(self, name : str = "Name", age : int = 0):
         self.__age = age
         self.__name = name
-
-    @overload
-    def __init__(self, name : str = "Jonatan"):
-        self.__init__(name, 20)
 
     def __del__(self):
         pass

742aac45d283ed5b5264f3e3ee001f26f06f624f
Семен Журавлев <zsocom@mail.ru>
2025-06-17 21:47:21 +0300

    Сериализация и Десиарилизация - JSON и Pickle

--- src/classwork/main.py
+++ src/classwork/main.py
@@ -1,28 +1,37 @@
-# Задание
-# Строительная компания нуждается в программе, которая бы по хотелкам клиента
-# прикидывала смету и итоговую стоимость проекта
-# Существуют следующие классы
-# 1. Подвал
-#   - Тип фундамента
-#   - Подводимые коммуникации
-#   - Площадь
-#   - Мощность входной электрики
-# 2. Этаж
-#   - Площадь
-#   - Кол-во комнат
-#   - Кол-во санузлов
-#   - Мощность входной электрики
-# 3. Крыша
-#   - Наличие и площадь Чердака
-#   - Тип крыши
-#   - Спутниковая тарелка
-#   - Мощность входной электрики
-# 4. Дополнительные постройки на территории
-#   - Площадь построек
-#   - Виды построек
-#   - Мощность входной электрики
-# 5. Дополнительные опции
-#   - Виды опций
-# Необходимо создать класс, который бы позволял по желанию клиента
-# сконфигурировать объект Дом, внутри которого будут находится поля-объекты
-# приведенных выше классов
+# Сериализация и Десериализация данных
+# Виды сериализаций:
+# 1. Бинарная сериализация данных - данные собираются в некую последовательную
+# структуру состящую из чисто байтовой последовательности
+# 2. Текстовая сериализация данных - XML -> JSON
+
+import json
+
+file = open("example.json", "r", encoding="utf-8")
+json_obj = json.load(file)
+file.close()
+
+print(json_obj["total"]["key_array"][3]["key_1"])
+
+json_obj["total"]["key_array"][3]["key_1"] = "value"
+print(json_obj["total"]["key_array"][3]["key_1"])
+
+file = open("example.json", "w", encoding="utf-8")
+json.dump(json_obj, file, ensure_ascii=False, indent=4)
+file.close()
+JSON = (json.dumps(json_obj, ensure_ascii=False)).replace(" ","")
+print(JSON)
+print(type(JSON))
+json_object = json.loads(JSON)
+print(json_object)
+print(type(json_object))
+
+import pickle
+file = open("example_binary","wb")
+pickle.dump(json_object, file,4)
+file.close()
+file = open("example_binary","rb")
+binary_file = pickle.load(file)
+print(binary_file)
+file.close()
+
+

bfbb3b050e2261421e18be60eeb58aa98b2ef12b
Семен Журавлев <zsocom@mail.ru>
2025-06-19 21:53:36 +0300

    Введение в базы данных

---
+++ database/template scripts/DDL operators.sql
@@ -0,0 +1,8 @@
+-- DROP TABLE public.example;
+-- DROP TABLE <название таблицы>
+
+CREATE TABLE public.example
+-- CREATE TABLE <название таблицы> (<список атрибутов>)
+(
+--  <имя> <тип данных> <характеристики> 
+)

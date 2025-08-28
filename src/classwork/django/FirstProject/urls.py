79e884391d9a043265ae31283d216ca5ad10e314
Семен Журавлев <zsocom@mail.ru>
2025-08-28 21:40:27 +0300

    HTTP - объяснение протокола

--- src/classwork/django/FirstProject/urls.py
+++ src/classwork/django/FirstProject/urls.py
@@ -1,8 +1,39 @@
-from django.urls import path
+from django.urls import path, re_path
 import hello_world.views as view
-
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path("", view.index),
     path("hello", view.hello_world),
+    path("about", view.hello_world),
+    re_path(r"^about", view.about_us),
 ]
+
+
+# scheme: схема запроса (http или https)
+# body: представляет тело запроса в виде строки байтов
+# path: представляет путь запроса
+# method: метод запроса (GET, POST, PUT и т.д.)
+# encoding: кодировка
+# content_type: тип содержимого запроса (значение заголовка CONTENT_TYPE)
+#
+# GET: объект в виде словаря, который содержит параметры запроса GET
+# POST: объект в виде словаря, который содержит параметры запроса POST
+# COOKIES: отправленные клиентом куки
+# FILES: отправленные клиентом файлы
+# META: хранит все доступные заголовки http в виде словаря. Набор заголовков зависит от клиента и сервера, некоторые из них:
+# CONTENT_LENGTH: длина содержимого.
+# CONTENT_TYPE: MIME-тип запроса.
+# HTTP_ACCEPT: типы ответа, которые принимает клиент.
+# HTTP_ACCEPT_ENCODING: кодировка, в которой клиент принимает ответ.
+# HTTP_ACCEPT_LANGUAGE: язык ответа, который принимает клиент.
+# HTTP_HOST: хост сервера.
+# HTTP_REFERER: страница, с которой клиент отправил запрос (при ее наличии).
+# HTTP_USER_AGENT: юзер-агент или информация о браузере клиента.
+# QUERY_STRING: строка запроса.
+# REMOTE_ADDR: IP-адрес клиента.
+# REMOTE_HOST: имя хоста клиента.
+# REMOTE_USER: аутентификационные данные клиента (при наличии)
+# REQUEST_METHOD: тип запроса (GET, POST).
+# SERVER_NAME: имя хоста сервера.
+# SERVER_PORT: порт сервера.
+# headers: заголовки запроса в виде словаря
--- src/classwork/django/hello_world/views.py
+++ src/classwork/django/hello_world/views.py
@@ -1,11 +1,21 @@
 # from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
+
 
 def index(request):
-    return HttpResponse(f"<h1>Это страница исходная</h1>"
-                        f"<a href={"hello"}>Перейти на страницу Hello</a>")
+    response = HttpResponse()
+    response.write(f"<h1>Это страница исходная</h1>"
+                   f"<a href={"hello"}>Перейти на страницу Hello</a>"
+                   f"</br>"
+                   f"<a href={"about"}>Перейти на страницу About</a>")
+    # response.status_code = 201
+    # response.headers["content-type"] = "text/html; charset=utf-8"
+    return response
 
 def hello_world(request):
     return HttpResponse(f"<h1>Hello World</h1>"
                         f"<a href={"../"}>Назад</a>")
 
+def about_us(request):
+    return HttpResponse(f"<h1 style='color:red;'>Hello On Our Site!</h1>"
+                        f"<a href={"../"}>Назад</a>")
